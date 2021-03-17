3070e7783675d482d628c8a2fcd97e90077e9ad7
Hashir-Durrani <muhammad.hashir.durrani@ocsbstudent.ca>
2021-03-17 17:50:36 +0000

    Made changes to CSS, Java Script, and HTML based on Linter results.

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 document.getElementById('pic').addEventListener('click', image)
 document.getElementById('apology').addEventListener('click', puns)
-document.getElementById('majic').addEventListener('click',carGone)
+document.getElementById('majic').addEventListener('click', carGone)
 document.getElementById('majic2').addEventListener('click', carBack)
 document.getElementById('back').addEventListener('click', backDrop)
 /* The commands above grab the button elements with the specified ids from the HTML file. The event listener prepares to apply functions to that button but it will wait until the button has been clicked. */
 
 function image () {
   document.getElementById('dog').src = 'images/cat.jpeg'
-  document.getElementById('changeText').innerHTML = 'I guess not. I figured you were more of a cat person'
+  document.getElementById('change-text').innerHTML = 'I guess not. I figured you were more of a cat person'
 }
 /* The lines of code above tell the corresponding event listeners to apply the functions in the curly brackets once the button has been clicked. In this case, the code is telling the dog image to change into a cat image and for some text to appear under it when the button is clicked. */
 
 function puns () {
   document.getElementById('dog').src = 'images/dog.jpeg'
-  document.getElementById('changeText').innerHTML = 'Sorry about that. I was just <i>kitten</i> around. But I guess that comment was a little <i>ruff</i>. All puns intended.'
+  document.getElementById('change-text').innerHTML = 'Sorry about that. I was just <i>kitten</i> around. But I guess that comment was a little <i>ruff</i>. All puns intended.'
 }
 /* This command tells the dog image to come back and changes the text under the image to what is written in those curly brackets after the innerHTML. */
 
